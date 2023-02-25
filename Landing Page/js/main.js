5b9fdf80a35713e9c4e39a3258809cd3066a10f4
Naiwrit Mullick <naiwrit.wrick@gmail.com>
2023-02-25 17:34:12 +0530

    Built the home page for LeadHer

--- Landing Page/js/main.js
+++ Landing Page/js/main.js
@@ -79,3 +79,13 @@
     
 })(jQuery);
 
+var clicks = 0;
+
+document.getElementById("clicks").innerHTML = clicks;
+
+$('.like-counter').click(function() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  $('.like-counter').addClass("liked");
+});
+
